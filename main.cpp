547ae5e07bc949f54914c455655b01248a193e10
MiguelPomboDias <miguel.pombodias@heig-vd.ch>
2017-03-17 14:25:55 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Miguel-Portable
+ *
+ * Created on 17. mars 2017, 14:23
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
