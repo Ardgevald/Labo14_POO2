618961b1ddbad8ff128d4863301843d4930e1168
MiguelPomboDias <miguel.pombodias@heig-vd.ch>
2017-03-17 14:37:12 +0100

    init classe String

---
+++ cstring.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   cstring.cpp
+ * Author: Miguel-Portable
+ * 
+ * Created on 17. mars 2017, 14:34
+ */
+
+#include "cstring.h"
+
+String::String() {
+}
+
+String::String(const String& orig) {
+}
+
+String::~String() {
+}
+
---
+++ cstring.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   cstring.h
+ * Author: Miguel-Portable
+ *
+ * Created on 17. mars 2017, 14:34
+ */
+
+#ifndef CSTRING_H
+#define	CSTRING_H
+
+class String {
+public:
+    String();
+    String(const String& orig);
+    virtual ~String();
+private:
+
+};
+
+#endif	/* CSTRING_H */
+
